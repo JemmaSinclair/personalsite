3b631760ce4e52555b928905703ca21836397b6b
jemmasinclair <therealglitchxy@gmail.com>
2024-09-04 01:08:34 -0400

    things

--- app.js
+++ app.js
@@ -19,8 +19,9 @@ app.use(session({
     saveUninitialized: true
 }));
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     const user = req.session.user;
